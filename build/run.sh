6e22ce211b34c00ed705d73d65d9ab7a1eb41967
NicolaPoerio <nicola.poerio@crf.it>
2020-11-03 22:55:42 +0100

    new download

--- build/run.sh
+++ build/run.sh
@@ -1,1 +1,1 @@
-sudo docker run -v /home/ictadmin/Documents/NP/CarND-ObjectDetectionUrban:/app/project/ -ti -p 8888:8888 --shm-size=1g --gpus all project-dev bash
+sudo docker run -v /home/ictadmin/Documents/NP/CarND-ObjectDetectionUrban:/app/project/ -ti -p 8888:8888 --gpus all --shm-size=1g  project-dev bash
